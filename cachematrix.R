0af2cc88aa9cf557b090ee3b19960f3d0f341552
Data Enthusiast <joshua@dataenthusiast.com>
2014-04-27 19:31:20 -0700

    ProgAssign2 - Final - Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## In this script there are two functions.
+##      1. makeCacheMatrix()
+##      2. cacheSolve()
+## Written by Joshua Burkhow 4/27/2014
+##
+## makeCacheMatrix is a function that either takes in a matrix class variable and "sets" it or returns a matrix value
+## that was previously set. 
 
 makeCacheMatrix <- function(x = matrix()) {
     
-    if(!(exists('y'))){
-        message("Caching Matrix")
-    } else {
-        message("Getting Cached Matrix")
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
     }
+    get <- function() {
+        x
+    }
+    setMatrix <- function(matrix) {
+        m <<- matrix
+    }
+    getMatrix <- function() {
+        m
+    }
+        
+    list(set = set, get = get, setMatrix = setMatrix, getMatrix = getMatrix)
     
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes in a value that was previously set using the makeCacheMatrix function. It then checks to see if
+## it had cached the answer before and if not it then returns the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    solve(x)
+    
+    m <- x$getMatrix()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setMatrix(m)
+    m
+    
 }
